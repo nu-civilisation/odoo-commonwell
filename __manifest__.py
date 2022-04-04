cbc1f482923428cddfbbf3cf083aa0017f6e1eaf
nu-civilisation <verein@nu-civilisation.org>
2022-04-04 16:40:47 +0200

    models tested

--- __manifest__.py
+++ __manifest__.py
@@ -5,6 +5,10 @@
     'author': "nu-civilisation",
     'category': 'nu',
     'data': [
+        'views/commonwell_partner_types.xml'
+        'views/commonwell_partner_tags.xml'
+        'views/commonwell_partners.xml'
+        'views/commonwell_menu.xml'
         'security/ir.model.access.csv',
     ]
 }
--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Developed by nu-civilisation. See LICENSE file for full copyright and licensing details.
 
-from . import res_partner
 from . import res_partner_tags
 from . import res_partner_types
+from . import res_partner
 from . import res_users
